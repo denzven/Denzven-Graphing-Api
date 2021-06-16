4704bfba6cde53ca0f011a9e7c15d3ec19d518fa
Denzven <69366709+denzven@users.noreply.github.com>
2021-06-16 10:06:10 +0530

    'first'

--- app.py
+++ app.py
@@ -10,8 +10,9 @@ def homepage():
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
 
-    <img src="http://loremflickr.com/600/400">
+    <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
+
